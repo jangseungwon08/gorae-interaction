c34d1ee6de177fb52d297b4cbbf2c1b6fe69bc65
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-19 17:59:28 +0900

    init

---
+++ gorae_interaction/src/main/java/com/gorae/gorae_interaction/GoraeInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.gorae.gorae_interaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoraeInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoraeInteractionApplication.class, args);
+	}
+
+}
---
+++ gorae_interaction/src/test/java/com/gorae/gorae_interaction/GoraeInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorae.gorae_interaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoraeInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
